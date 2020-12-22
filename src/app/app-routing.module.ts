f9107d09984476762c3e50ea6de0c48710ad9a71
khurram khan <khuram.k@emblemtechnologies.com>
2020-12-22 18:39:29 +0500

    first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StepperComponent } from './stepper/stepper.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'stepper', component: StepperComponent},
+
+
+  {path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
